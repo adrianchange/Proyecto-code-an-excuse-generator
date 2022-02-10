75af3c32ed1f6fdcb5337dc276450c960788b734
adrianchange <adrianchange@gmail.com>
2022-02-10 20:59:51 +0000

    Finished my project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello!");
+};
+let generateExcuse = () => {
+  let pron = ["The", "A", "My", "Your"];
+  let sub = ["cat", "storm", "mom", "fish"];
+  let act = ["ruined", "distroyed", "smashed", "broke"];
+  let what = ["my life", "my project", "the world", "the house"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronIndx = Math.floor(Math.random() * pron.length);
+  let subIndx = Math.floor(Math.random() * sub.length);
+  let actIndx = Math.floor(Math.random() * act.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  return (
+    pron[pronIndx] +
+    " " +
+    sub[subIndx] +
+    " " +
+    act[actIndx] +
+    " " +
+    what[whatIndx]
+  );
 };
